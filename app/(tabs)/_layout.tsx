957642715e3facf89f7612b8ab5aef200fa4a255
Nnaemeka Onukwube <emmycontacts@gmail.com>
2024-08-27 00:49:53 +0100

    complete UI

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,34 +1,40 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs } from "expo-router";
+import React from "react";
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: false,
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+          title: "",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={focused ? "stats-chart" : "stats-chart-outline"}
+              color="black"
+              size={28}
+            />
           ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="profile"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+          title: "",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <MaterialCommunityIcons
+              name={focused ? "account" : "account-outline"}
+              size={28}
+              color={"#000"}
+            />
           ),
         }}
       />
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,188 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import {
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React from "react";
+import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
+import { LineChart } from "react-native-gifted-charts";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const HomeScreen = () => {
+  const data = [{ value: 15 }, { value: 30 }, { value: 26 }, { value: 40 }];
 
-export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.topWrapper}>
+        <Text style={styles.topWrapperText}>Let's start your day</Text>
+
+        <TouchableOpacity
+          style={{
+            position: "relative",
+          }}
+        >
+          <Image
+            style={{ height: 50, width: 50 }}
+            source={require("../../assets/images/profile.png")}
+          />
+          <View style={styles.notifier} />
+        </TouchableOpacity>
+      </View>
+
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "flex-start",
+          paddingHorizontal: 20,
+          marginTop: 18,
+        }}
+      >
+        <Text style={{ fontSize: 16, fontWeight: "600", marginBottom: 15 }}>
+          Your Report
+        </Text>
+        <View style={styles.reportWrapper}>
+          <View style={styles.reportContent}>
+            <MaterialIcons name="directions-walk" size={35} color="black" />
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "flex-start",
+              }}
+            >
+              <Text style={{ fontSize: 14, fontWeight: "500" }}>324mph</Text>
+              <Text style={{ fontSize: 14, fontWeight: "300" }}>Walking</Text>
+            </View>
+          </View>
+
+          <View style={styles.reportContent}>
+            <MaterialCommunityIcons name="fire" size={35} color="black" />
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "flex-start",
+              }}
+            >
+              <Text style={{ fontSize: 14, fontWeight: "500" }}>288 c</Text>
+              <Text style={{ fontSize: 14, fontWeight: "300" }}>
+                Temperature
+              </Text>
+            </View>
+          </View>
+
+          <View style={styles.reportContent}>
+            <MaterialCommunityIcons
+              name="battery-high"
+              size={35}
+              color="black"
+            />
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "flex-start",
+              }}
+            >
+              <Text style={{ fontSize: 14, fontWeight: "500" }}>58%</Text>
+              <Text style={{ fontSize: 14, fontWeight: "300" }}>Battery</Text>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.chartWrapper}>
+          <Text style={{ fontSize: 16, fontWeight: "600", marginBottom: 15 }}>
+            Navigation Chart
+          </Text>
+          <View style={{ marginTop: 5 }}>
+            <LineChart
+              data={data}
+              color={"#177AD5"}
+              width={300}
+              thickness={3}
+              dataPointsColor={"#000"}
+              showArrows={true}
+            />
+          </View>
+        </View>
+
+        <View style={styles.footer}>
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between" }}
+          >
+            <Text style={styles.footerText}>Emergency Contact</Text>
+            <Text style={[styles.footerText, { color: "#2F80ED" }]}>
+              Change
+            </Text>
+          </View>
+          <TouchableOpacity
+            style={{
+              padding: 20,
+              backgroundColor: "#F2F2F2",
+              borderRadius: 20,
+            }}
+          >
+            <Text style={{ fontSize: 24, fontWeight: "600" }}>
+              080776654340
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
   );
-}
+};
+
+export default HomeScreen;
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: { flex: 1, backgroundColor: "#fff" },
+  topWrapper: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+    alignItems: "center",
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  topWrapperText: { fontSize: 24, fontWeight: "700" },
+  notifier: {
+    position: "absolute",
+    padding: 5,
+    backgroundColor: "#2F80ED",
+    borderRadius: 50,
+    right: 3,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  footer: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    paddingHorizontal: 5,
+    marginTop: 28,
+  },
+  footerText: { fontSize: 16, fontWeight: "600", marginBottom: 15 },
+  chartWrapper: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    paddingHorizontal: 5,
+    marginTop: 18,
+  },
+  reportContent: {
+    display: "flex",
+    flexDirection: "row",
+    gap: 3,
+    alignItems: "center",
+  },
+  reportWrapper: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    borderWidth: 0.9,
+    borderColor: "#a3a3a3",
+    borderRadius: 15,
+    paddingVertical: 30,
   },
 });
